c877a36b442b04671c251313ce7175d1135d1b23
unknown <eddiluan45@gmail.com>
2023-02-25 09:44:51 +0800

    Login

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+  <meta http-equiv="x-ua-compatible" content="ie=edge" />
+  <title>SIP - Login</title>
+  <!-- MDB icon -->
+  <link rel="icon" href="img/mdb-favicon.ico" type="image/x-icon" />
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.2/css/all.css" />
+  <!-- Google Fonts Roboto -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap" />
+  <!-- MDB -->
+  <link rel="stylesheet" href="assets/css/bootstrap-login-form.min.css" />
+</head>
+
+<body>
+  <!-- Start your project here-->
+  <section class="vh-100">
+    <div class="container py-5 h-100">
+      <div class="row d-flex justify-content-center align-items-center h-100">
+        <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+          <div class="card shadow-2-strong" style="border-radius: 1rem;">
+            <div class="card-body p-5 text-center">
+                <img src="assets/logo.png" width="140px">
+                <h4 class="mb-5">Sistem Informasi Pengarsipan <br> Kelurahan Lasiana</h4>
+                <br>
+             
+  
+  
+                <h5>Login Akun Kamu</h5> 
+                <hr class="my-4">
+                <button class="btn btn-lg btn-block btn-primary" style="background-color: #3b5998;" type="submit">Login Petugas Penerima Tamu</button>
+                <button class="btn btn-lg btn-block btn-primary" style="background-color: #3b5998;" type="submit">Login Petugas Operator</button>
+                <button class="btn btn-lg btn-block btn-primary" style="background-color: #3b5998;" type="submit">Login Petugas Pegarsipan</button>
+  
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- End your project here-->
+
+  <!-- MDB -->
+  <script type="text/javascript" src="assets/js/mdb.min.js"></script>
+  <!-- Custom scripts -->
+  <script type="text/javascript"></script>
+</body>
+
+</html>
